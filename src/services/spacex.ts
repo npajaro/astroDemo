036f0a3603ee04cadf9975ca0826e804b2ff881d
Nehemias Pajaro <npajaro1@gmail.com>
2023-11-18 16:04:44 -0500

    first commit

---
+++ src/services/spacex.ts
@@ -0,0 +1,32 @@
+import { type APISpaceXResponse, type Doc } from '../types/apiResponse';
+
+
+export const gestLaunchBy = async ({id}: {id: string}) => {
+
+    const res = await fetch('https://api.spacexdata.com/v5/launches/${id}');
+
+    const launch = (await res.json()) as Doc;
+    return launch;
+}
+
+export const gestLatestLaunches = async () => {
+
+    const res = await fetch('https://api.spacexdata.com/v5/launches/query', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json' 
+        },
+        body: JSON.stringify({
+            query: {},
+            options: {
+                sort: {
+                    date_unix: 'asc'
+                },
+                limit: 18
+            },
+        }),
+    });
+
+    const {docs: launches} = await res.json() as APISpaceXResponse;
+    return launches;
+}
