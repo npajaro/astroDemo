eda958c6b7bacdbe179949a3eb3c521c00fa2ef5
Nehemias Pajaro <npajaro1@gmail.com>
2023-11-20 20:03:20 -0500

    page static BASE_URL

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
+export const BASE_URL = '/astroDemo';
+
 
 // la url de acceso a mi repo es https://npajaro.github.io/astroDemo/
 // por lo tanto, la base es /astroDemo
@@ -11,5 +13,5 @@ export default defineConfig({
   integrations: [tailwind()],
   // output: 'server',
   site: 'https://npajaro.github.io',
-  base: '/astroDemo',
+  base: BASE_URL,
 });
